495d74b47225121b93dcb618581d5c660c1ea49e
Ruslan P <111646522+nikopolzp@users.noreply.github.com>
2023-02-14 00:13:33 +0200

    add marking html from js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+console.log(makePhotoSet(galleryItems));
+
+const photoContainer = document.querySelector(".gallery");
+const photoMarkup = makePhotoSet(galleryItems);
+photoContainer.insertAdjacentHTML("beforeend", photoMarkup);
+photoContainer.addEventListener("click", onphotoContainerClick);
+
+function makePhotoSet(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+function onphotoContainerClick(e) {}
