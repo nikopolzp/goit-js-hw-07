68357267d8d29eae89eb2337ba3e6a26e27fe43c
Ruslan P <111646522+nikopolzp@users.noreply.github.com>
2023-02-18 19:10:01 +0200

    finish tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,28 +1,47 @@
 import { galleryItems } from "./gallery-items.js";
+
+console.log(galleryItems);
 // Change code below this line
 
-console.log(makePhotoSet(galleryItems));
+const gallery = document.querySelector(".gallery");
+const items = [];
 
-const photoContainer = document.querySelector(".gallery");
-const photoMarkup = makePhotoSet(galleryItems);
-photoContainer.insertAdjacentHTML("beforeend", photoMarkup);
-photoContainer.addEventListener("click", onphotoContainerClick);
+galleryItems.forEach((element) => {
+  const galleryItem = document.createElement("div");
+  galleryItem.className = "gallery__item";
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__link";
+  galleryLink.href = element.original;
+  const galleryImage = document.createElement("img");
+  galleryImage.className = "gallery__image";
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute("data-source", element.original);
+  galleryImage.alt = element.description;
 
-function makePhotoSet(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-        <a class="gallery__link" href="large-image.jpg">
-          <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </div>`;
-    })
-    .join("");
-}
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+  items.push(galleryItem);
+});
 
-function onphotoContainerClick(e) {}
+gallery.append(...items);
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const selectedImage = e.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`);
+
+  instance.show();
+
+  gallery.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  });
+});
